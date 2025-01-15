4710c1d2f14c1f265049b27d75f9d9e09a16be12
jaredb <jaredb@mit.edu>
2025-01-15 10:52:31 -0500

    hyperparameter grid config file fix

--- create_configs.py
+++ create_configs.py
@@ -5,69 +5,70 @@ import itertools
 from data_dir.project_dir import get_linoss_directory
 
 
-def write_config(
-    in_filename,
-    out_filename,
+def create_configs(
+    in_dir,
+    out_dir,
     model_name,
     dataset_name,
-    learning_rate,
-    hidden_dim,
-    state_dim,
-    blocks,
-    time,
-    discretization,
-    damping,
+    params={},
 ):
-    with open(in_filename, 'r') as in_file:
-        data = json.load(in_file)
+    labels = params.keys()
+    combos = itertools.product(params.values())
+    for i, combo in enumerate(combos):
+        # I/O
+        in_filename = in_dir / model / (dataset + ".json")
+        os.makedirs(out_dir / model / dataset , exist_ok=True)
+        out_filename = out_dir / model / dataset / f"config_{i:03}.json"
+        
+        # Read/write config
+        with open(in_filename, "r") as in_file:
+            data = json.load(in_file)
 
-    data['dataset_name'] = dataset_name
-    data['model_name'] = model_name
-    data['linoss_discretization'] = str(discretization)
-    data['damping'] = bool(damping)
-    data['lr'] = str(learning_rate)
-    data['hidden_dim'] = str(hidden_dim)
-    data['ssm_dim'] = str(state_dim)
-    data['num_blocks'] = str(blocks)
-    data['time'] = str(time)
-    data['parameterization'] = str(parameterization)
+        data["dataset_name"] = dataset_name
+        data["model_name"] = model_name
+        sample_data = {labels[i]: combo[i] for i in range(len(combo))}
+        data = data | sample_data
 
-    with open(out_filename, 'w') as out_file:
-        json.dump(data, out_file, indent=4)
+        with open(out_filename, "w") as out_file:
+            json.dump(data, out_file, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Input / output config files
     linoss_dir = get_linoss_directory()
-    in_dir = linoss_dir / 'experiment_configs' / 'repeats' 
-    out_dir = linoss_dir / 'experiment_configs' / 'grid'
+    in_dir = linoss_dir / "experiment_configs" / "repeats" 
+    out_dir = linoss_dir / "experiment_configs" / "grid"
 
     # Models & Datasets
     models = ["LinOSS"]
     datasets = ["ppg"]
 
     # Enumerate hyperparameter grid
-    learning_rates = [1e-3, 1e-4, 1e-5]
+    learning_rates = [1e-5, 1e-2]
     hidden_dims = [16, 64, 128]
-    state_dims = [16, 64, 256]
+    state_dims = [16, 64, 128, 256]
     blocks = [2, 4, 6]
     time = [False, True]
-    discretization = ['IMEX']
+    discretization = ["IMEX"]
     damping = [True]
-    parameterization = ['stable']
+    parameterization = ["stable"]
 
     # Write configuration files
     for dataset in datasets:
         for model in models:
-            params = itertools.product(learning_rates, hidden_dims, state_dims, blocks, time, discretization, damping, parameterization)
-            for i, combo in enumerate(params):
-                in_filename = in_dir / model / (dataset + '.json')
-                os.makedirs(out_dir / f'config_{i:03}' / model, exist_ok=True)
-                out_filename = out_dir / f'config_{i:03}' / model / (dataset + '.json')
-                write_config(
-                    in_filename,
-                    out_filename,
-                    model,
-                    dataset,
-                    *combo,
-                )
+            create_configs(
+                in_dir,
+                out_dir,
+                model,
+                dataset,
+                params={
+                    "lr": learning_rates,
+                    "hidden_dim": hidden_dims, 
+                    "ssm_dim": state_dims, 
+                    "num_blocks": blocks, 
+                    "time": time, 
+                    "linoss_discretization": discretization, 
+                    "damping": damping,
+                    "parameterization": parameterization,
+                },
+            )
